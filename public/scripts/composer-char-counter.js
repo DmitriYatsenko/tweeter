d7b68112e579940b07b94119d64a5c7b8c853ae8
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2022-01-05 16:59:42 +0000

    error message disappears after 2 seconds

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,20 +7,42 @@ $(document).ready(function () {
         let tooLong = wordLength > remainingChars;
 
         if (htmlTags && tooLong) {
+            // Display error message & character counter in red if tweet is too long & contains HTML tags
+            $('#counter').css({ color: 'red' });
             $('.error-text').text('Must be 140 characters or less & not contain HTML tags');
             $('.error-text').css({ color: 'red' });
-            $('#counter').css({ color: 'red' });
+
+            // Error message disappears after 2 seconds
+            setTimeout(() => {
+                $('.error-text').text('');
+            }, 2000);
+
         } else if (!htmlTags && tooLong) {
+            // Display character counter in red if tweet is too long
+            $('#counter').css({ color: 'red' });
             $('.error-text').text('Must be 140 characters or less');
             $('.error-text').css({ color: 'red' });
-            $('#counter').css({ color: 'red' });
+
+            // Error message disappears after 2 seconds
+            setTimeout(() => {
+                $('.error-text').text('');
+            }, 2000);
+
         } else if (htmlTags && !tooLong) {
+            // Display error message in red if tweet contains HTML tags
+            $('#counter').css({ color: 'black' });
             $('.error-text').text('Must not contain HTML tags');
             $('.error-text').css({ color: 'red' });
-            $('#counter').css({ color: 'black' });
+
+            // Error message disappears after 2 seconds
+            setTimeout(() => {
+                $('.error-text').text('');
+            }, 2000);
+
         } else {
-            $('.error-text').text('');
+            // Display character counter normally (in black) if tweet isn't too long & doesn't contain HTML tags
             $('#counter').css({ color: 'black' });
+            $('.error-text').text('');
         }
         $('#counter').text(remainingChars - wordLength);
     })
